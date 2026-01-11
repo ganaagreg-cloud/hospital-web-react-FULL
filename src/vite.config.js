632ab2c4a8bf5fd85e8a179c77d06142f4b9eea3
ganaagreg-cloud <ganaagreg@gmail.com>
2026-01-11 14:31:22 +0800

    awad

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hospital-web-react-FULL/" // 👈 MUST match your repo name
+  base: "/hospital-web-react-FULL/"
 });
